e1a43d92b769be56d29f8e87c7f09c86260b4b5d
tedha012 <tedha012@gmail.com>
2022-10-21 12:22:08 +0900

    #5.0 Introduction

--- houses/models.py
+++ houses/models.py
@@ -1,15 +1,28 @@
 from email.headerregistry import Address
 from email.policy import default
 from pydoc import describe
+from tabnanny import verbose
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+
 class House(models.Model):
 
-  """ Model Definition for Houses """
-  name = models.CharField(max_length=140)
-  price_per_night = models.PositiveIntegerField()
-  description = models.TextField()
-  Address = models.CharField(max_length=140)
-  pet_allowed = models.BooleanField(default=True)
+    """ Model Definition for Houses """
+    name = models.CharField(max_length=140)
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="Price",
+        help_text="Positive Numbers Only"
+    )
+    description = models.TextField()
+    address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(
+        verbose_name="Pets Allowed?",
+        default=True,
+        help_text="Does this house allow pets?"
+    )
+
+    def __str__(self):
+        return self.name
