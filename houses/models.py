61432c58b27668600693bb15302423ccedc9a764
tedha012 <tedha012@gmail.com>
2022-10-21 09:51:33 +0900

    #4.1 Migrations

--- houses/models.py
+++ houses/models.py
@@ -1,4 +1,5 @@
 from email.headerregistry import Address
+from email.policy import default
 from pydoc import describe
 from unittest.util import _MAX_LENGTH
 from django.db import models
@@ -8,6 +9,7 @@ class House(models.Model):
 
   """ Model Definition for Houses """
   name = models.CharField(max_length=140)
-  price = models.PositiveIntegerField()
+  price_per_night = models.PositiveIntegerField()
   description = models.TextField()
   Address = models.CharField(max_length=140)
+  pet_allowed = models.BooleanField(default=True)
