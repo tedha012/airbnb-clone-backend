1b8ecd857ce95a0a5aeda538c9752f9f709476c3
TedHa <tedha012@gmail.com>
2022-10-22 15:38:46 +0900

    #6.2 Many to Many

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from common.models import CommonModel
 
 
-class Room(models.Model):
+class Room(CommonModel):
 
     """Room Model Definition"""
 
@@ -31,21 +32,28 @@ class Room(models.Model):
     rooms = models.PositiveIntegerField()
     toilets = models.PositiveIntegerField()
     description = models.TextField()
-    address = models.CharField(max_length=250)
-    pet_friendly = models.BooleanField(default=True)
+    address = models.CharField(
+        max_length=250,
+    )
+    pet_friendly = models.BooleanField(
+        default=True,
+    )
     kind = models.CharField(
         max_length=20,
         choices=RoomKindChoices.choices,
     )
     owner = models.ForeignKey(
-        "user.User",
+        "users.User",
         on_delete=models.CASCADE,
+    )
+    amenities = models.ManyToManyField(
+        "rooms.Amenity",
     )
 
 
-class Amenity(models.Model):
+class Amenity(CommonModel):
 
-    """Amenity Definition"""
+    """Amenity Definiton"""
 
     name = models.CharField(
         max_length=150,
