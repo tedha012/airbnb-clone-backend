c304d34ccb24082e6d4cbd114424e96d598aba89
TedHa <tedha012@gmail.com>
2022-10-22 12:44:33 +0900

    #5.5 Foreign Keys

--- houses/models.py
+++ houses/models.py
@@ -5,24 +5,23 @@ from tabnanny import verbose
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
-
 
 class House(models.Model):
 
-    """ Model Definition for Houses """
+    """Model Definition for Houses"""
+
     name = models.CharField(max_length=140)
     price_per_night = models.PositiveIntegerField(
-        verbose_name="Price",
-        help_text="Positive Numbers Only"
+        verbose_name="Price", help_text="Positive Numbers Only"
     )
     description = models.TextField()
     address = models.CharField(max_length=140)
     pets_allowed = models.BooleanField(
         verbose_name="Pets Allowed?",
         default=True,
-        help_text="Does this house allow pets?"
+        help_text="Does this house allow pets?",
     )
+    owver = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
