6a8ad39a8aa11d11585c3981755692e587716efd
TedHa <tedha012@gmail.com>
2022-10-22 13:02:25 +0900

    #5.6 Super Mega Recap

--- users/models.py
+++ users/models.py
@@ -4,7 +4,18 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=150, editable=False)
-    last_name = models.CharField(max_length=150, editable=False)
-    name = models.CharField(max_length=150, default="")
-    is_host = models.BooleanField(default=False)
+    first_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    name = models.CharField(
+        max_length=150,
+        default="",
+    )
+    is_host = models.BooleanField(
+        default=False,
+    )
