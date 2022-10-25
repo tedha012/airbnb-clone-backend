c5f88457d33eee6a1057ad6fe06cad9edec848ad
tedha012 <tedha012@gmail.com>
2022-10-25 14:39:40 +0900

    #10.8 DELETE

--- categories/views.py
+++ categories/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
 from .models import Category
 from .serializers import CategorySerializer
 
@@ -24,7 +25,7 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view(["GET", "PUT"])
+@api_view(["GET", "PUT", "DELETE"])
 def category(request, pk):
     try:
         category = Category.objects.get(pk=pk)
@@ -34,6 +35,7 @@ def category(request, pk):
     if request.method == "GET":
         serializer = CategorySerializer(category)
         return Response(serializer.data)
+
     elif request.method == "PUT":
         serializer = CategorySerializer(
             category,
@@ -45,3 +47,7 @@ def category(request, pk):
             return Response(CategorySerializer(updated_category).data)
         else:
             return Response(serializer.errors)
+
+    elif request.method == "DELETE":
+        category.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
